154d6880148725de4a9648c236a61eb581731bd8
enriquedlh97 <enriquedlh97@hotmail.com>
2021-08-26 01:55:42 -0500

    Updates model download script

--- bin/download_model.py
+++ bin/download_model.py
@@ -1,6 +1,12 @@
 import gdown
+import os
+
+
+if not os.path.exists('assets'):
+    os.makedirs('assets')
 
 print("\nThe model is about 2GB and will take some time to be downloaded\n", flush=True)
+
 gdown.download(
     "https://drive.google.com/uc?id=1-G0G63J3NBST2N76n0RnVKv9MttyoRJO",
     "assets/model_best_checkpoint.ckpt",
