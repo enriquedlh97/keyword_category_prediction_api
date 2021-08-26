a8b1103d448317cf1f3d98d0666fcefe5007cd53
enriquedlh97 <enriquedlh97@hotmail.com>
2021-08-25 22:24:31 -0500

    Adds file for abstracting preprocessing of requests and postprocessing of responses from the model

---
+++ keyword_category_predictor/models/model.py
@@ -0,0 +1,50 @@
+import json
+import torch
+from torch import nn
+from transformers import BertTokenizer
+from bert_multilingual import KeywordCategorizer
+
+with open("config.json") as json_file:
+    config = json.load(json_file)
+
+
+class Model:
+    def __init__(self):
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.tokenizer = BertTokenizer.from_pretrained(config["MODEL"])
+        category_predictor = KeywordCategorizer(len(config["CLASS_NAMES"]))
+        category_predictor.load_from_checkpoint(
+            config["PRE_TRAINED_MODEL"],
+            n_classes=len(config["CLASS_NAMES"])
+        )
+        self.category_predictor = category_predictor.to(self.device)
+
+    def predict(self, text):
+
+        encoded_text = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=config["MAX_SEQUENCE_LEN"],
+            return_token_type_ids=False,
+            padding="max_length",
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
+
+        input_ids = encoded_text["input_ids"].to(self.device)
+        attention_mask = encoded_text["attention_mask"].to(self.device)
+
+        confidence, prediction = self.category_predictor(input_ids, attention_mask)
+        prediction = prediction.flatten().numpy()
+
+        return (
+            confidence,
+            dict(zip(config["CLASS_NAMES"], prediction)),
+        )
+
+
+model = Model()
+
+
+def get_model():
+    return model
