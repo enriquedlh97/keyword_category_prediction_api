803329e30d97f547a119f9bbc5bc9cb57a761b74
enriquedlh97 <enriquedlh97@hotmail.com>
2021-08-26 23:00:43 -0500

    Adds script to download dataset

--- bin/download_dataset.py
+++ bin/download_dataset.py
@@ -1,9 +1,34 @@
 import gdown
+import tarfile
+import threading
+import os
 
 
-print("\nDownloading dataset\n", flush=True)
+def download_data():
 
-gdown.download(
-    "https://drive.google.com/uc?id=1LpTDSox94zjGV78hKoUYbT8KZv7CnqdD",
-    "dataset",
-)
+    if not os.path.exists('dataset'):
+        os.makedirs('dataset')
+
+    print("\nDownloading dataset\n", flush=True)
+
+    gdown.download(
+        "https://drive.google.com/uc?id=1LtrGndz9P766BRPf-jWkRw0_gzDuVCVo",
+        "dataset/keyword_categories.tar.gz",
+    )
+
+
+def main():
+    thread = threading.Thread(target=download_data)
+    thread.start()
+    # wait for the data to be downloaded
+    thread.join()
+
+    # Extract dataset
+    print("\nDownloading dataset\n", flush=True)
+    tar = tarfile.open("dataset/keyword_categories.tar.gz", "r:gz")
+    tar.extractall("dataset/keyword_categories/")
+    tar.close()
+
+
+if __name__ == '__main__':
+    main()
