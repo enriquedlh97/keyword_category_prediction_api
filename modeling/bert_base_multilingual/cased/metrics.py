85923ad5dc836b28b2d8ec7b04d677fce7b9fcad
enriquedlh97 <enriquedlh97@hotmail.com>
2021-08-26 20:23:42 -0500

    Fixes bug in functions for metric computations

--- modeling/bert_base_multilingual/cased/metrics.py
+++ modeling/bert_base_multilingual/cased/metrics.py
@@ -9,15 +9,15 @@ def mean_auc_roc(predictions, labels, label_columns):
 
     print("Average precision per tag")
     for i, name in enumerate(label_columns):
-        auc_roc_values = auroc(predictions[:, i], labels[:, i])
-        auc_roc_dict[name] = auc_roc_values.reshape(-1).numpy()[0]
-        auc_roc_values.append(auc_roc_values.reshape(-1).numpy()[0])
+        auc_roc = auroc(predictions[:, i], labels[:, i])
+        auc_roc_dict[name] = auc_roc.reshape(-1).numpy()[0]
+        auc_roc_values.append(auc_roc.reshape(-1).numpy()[0])
 
     return np.array(auc_roc_values).mean(), auc_roc_dict
 
 
 def mean_avg_precision(predictions, labels, label_columns):
-    avg_precision = []
+    avg_precision_values = []
     avg_precision_dict = {}
     average_precision = AveragePrecision(pos_label=1)
 
@@ -25,6 +25,6 @@ def mean_avg_precision(predictions, labels, label_columns):
     for i, name in enumerate(label_columns):
         avg_precision = average_precision(predictions[:, i], labels[:, i])
         avg_precision_dict[name] = avg_precision.reshape(-1).numpy()[0]
-        avg_precision.append(avg_precision.reshape(-1).numpy()[0])
+        avg_precision_values.append(avg_precision.reshape(-1).numpy()[0])
 
-    return np.array(avg_precision).mean(), avg_precision_dict
+    return np.array(avg_precision_values).mean(), avg_precision_dict
