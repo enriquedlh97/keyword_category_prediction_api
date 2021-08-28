cc5df3e78a3861a43325496062c04d45eddf3ab4
enriquedlh97 <enriquedlh97@hotmail.com>
2021-08-28 05:58:37 -0500

    Updates cofnig file and model download script

--- bin/download_model.py
+++ bin/download_model.py
@@ -6,7 +6,6 @@ if not os.path.exists('assets'):
     os.makedirs('assets')
 
 print("\nThe model is about 2GB and will take some time to be downloaded\n", flush=True)
-print("\nDownloading...\n", flush=True)
 
 gdown.download(
     "https://drive.google.com/uc?id=1JX5B6ctdzTSq97ddKMQZKOVegFJCk2tg",
