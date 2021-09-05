d6872290eeb80b391cc39c16f51b8f6513324419
enriquedlh97 <enriquedlh97@hotmail.com>
2021-09-04 19:16:24 -0500

    Adds script for bert hyperparameter optimization subroutine

---
+++ hyperparameter_optimization/bert_base_multilingual_cased/hyperparam_opt_bert_base_multilingual_cased.py
@@ -0,0 +1,62 @@
+from bayes_opt import BayesianOptimization
+from modeling.bert_base_multilingual.cased.hyperparameters.optimization_structure import evaluate_model
+
+# For saving progress
+from bayes_opt.logger import JSONLogger
+from bayes_opt.event import Events
+
+# Others
+import os
+import argparse
+import time
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--i', type=int, default=1, help="Set number of steps for random exploration")
+parser.add_argument('--n', type=int, default=2, help="Set number of steps for bayesian optimization")
+args = parser.parse_args()
+
+start_time = time.time()
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+# Set path to logs
+path_to_logs = 'logs'
+
+logger = JSONLogger(path="{path}/logs.json".format(path=path_to_logs))
+
+# Set exploration and exploitation parameters
+init_points = args.i
+n_iter = args.n
+
+# Bounded region of parameter space
+pbounds = {
+    'max_token_count': (33, 60),
+    'epochs': (1, 1.1),
+    'batch_size': (8, 128),
+    'learning_rate': (1e-4, 5e-5),
+    'dropout': (0, 1),
+    'learning_rate_schedule': (0, 1),
+}
+
+# Initialize optimizer
+optimizer = BayesianOptimization(
+    f=evaluate_model,
+    pbounds=pbounds,
+    verbose=2,  # verbose=1 prints only when a max is observed
+    random_state=1,
+)
+
+# Sets up logger
+optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
+
+# Optimize
+optimizer.maximize(
+    init_points=init_points,
+    n_iter=n_iter,
+)
+
+print(optimizer.max, flush=True)
+
+print("--- %s seconds ---" % (time.time() - start_time), flush=True)
