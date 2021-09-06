2a242bf34abf505af5b986edf1ae3037cd556c0b
enriquedlh97 <enriquedlh97@hotmail.com>
2021-09-06 14:31:22 -0500

    Updates bert model download script

--- bin/download_model.py
+++ bin/download_model.py
@@ -5,9 +5,12 @@ import os
 if not os.path.exists('assets'):
     os.makedirs('assets')
 
+if not os.path.exists('assets/bert'):
+    os.makedirs('assets/bert')
+
 print("\nThe model is about 2GB and will take some time to be downloaded\n", flush=True)
 
 gdown.download(
     "https://drive.google.com/uc?id=1fPqgyH8PFpur_r3oOPLfF3IEBTd4Zl_e",
-    "assets/best-checkpoint.ckpt",
+    "assets/bert/best-checkpoint.ckpt",
 )
