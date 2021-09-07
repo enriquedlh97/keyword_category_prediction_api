3618b0cc9be629c066e16bf57bcaec63b27488a5
enriquedlh97 <enriquedlh97@hotmail.com>
2021-09-06 22:57:15 -0500

    Updates script for training all baseline models

--- training_and_testing/train_test_baseline_models.sh
+++ training_and_testing/train_test_baseline_models.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-chmod u+x training_and_testing/train_baseline_models.sh;
 training_and_testing/train_baseline_models.sh;
 
-chmod u+x training_and_testing/test_baseline_models.sh;
 training_and_testing/test_baseline_models.sh;
