870b0e2c2d34fef48a762ec8bc72ee997effcdac
enriquedlh97 <enriquedlh97@hotmail.com>
2021-09-08 04:35:35 -0500

    Adds script to test optimize models for all categories for random forest

---
+++ hyperparameter_optimization/random_forest/optimize_hyperparams.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# "Health"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=0 --i=2 --n=5;
+
+# "Vehicles"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=1 --i=2 --n=5;
+
+# "Hobbies & Leisure"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=2 --i=2 --n=5;
+
+# "Food & Groceries"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=3 --i=2 --n=5;
+
+# "Retailers & General Merchandise"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=4 --i=2 --n=5;
+
+# "Arts & Entertainment"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=5 --i=2 --n=5;
+
+# "Jobs & Education"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=6 --i=2 --n=5;
+
+# "Law & Government"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=7 --i=2 --n=5;
+
+# "Home & Garden"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=8 --i=2 --n=5;
+
+# "Finance"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=9 --i=2 --n=5;
+
+# "Computers & Consumer Electronics"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=10 --i=2 --n=5;
+
+# "Internet & Telecom"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=11 --i=2 --n=5;
+
+# "Sports & Fitness"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=12 --i=2 --n=5;
+
+# "Dining & Nightlife"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=13 --i=2 --n=5;
+
+# "Business & Industrial"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=14 --i=2 --n=5;
+
+# "Occasions & Gifts"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=15 --i=2 --n=5;
+
+# "Travel & Tourism"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=16 --i=2 --n=5;
+
+# "News, Media & Publications"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=17 --i=2 --n=5;
+
+# "Apparel"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=18 --i=2 --n=5;
+
+# "Beauty & Personal Care"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=19 --i=2 --n=5;
+
+# "Family & Community"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=20 --i=2 --n=5;
+
+# "Real Estate"
+python hyperparameter_optimization/random_forest/hyperparam_opt_random_forest.py --c=21 --i=2 --n=5;
