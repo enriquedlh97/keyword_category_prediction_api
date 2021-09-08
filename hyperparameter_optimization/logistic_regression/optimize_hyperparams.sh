27a64f898d3bea0ec9fd45cfa1ada8e5972e66d2
enriquedlh97 <enriquedlh97@hotmail.com>
2021-09-08 03:57:55 -0500

    Finishes hyperparameter optimization subroutine for logistic regression

--- hyperparameter_optimization/logistic_regression/optimize_hyperparams.sh
+++ hyperparameter_optimization/logistic_regression/optimize_hyperparams.sh
@@ -1,67 +1,67 @@
 #!/bin/bash
 
 # "Health"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=0 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=0 --i=20 --n=200;
 
 # "Vehicles"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=1 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=1 --i=20 --n=200;
 
 # "Hobbies & Leisure"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=2 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=2 --i=20 --n=200;
 
 # "Food & Groceries"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=3 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=3 --i=20 --n=200;
 
 # "Retailers & General Merchandise"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=4 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=4 --i=20 --n=200;
 
 # "Arts & Entertainment"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=5 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=5 --i=20 --n=200;
 
 # "Jobs & Education"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=6 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=6 --i=20 --n=200;
 
 # "Law & Government"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=7 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=7 --i=20 --n=200;
 
 # "Home & Garden"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=8 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=8 --i=20 --n=200;
 
 # "Finance"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=9 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=9 --i=20 --n=200;
 
 # "Computers & Consumer Electronics"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=10 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=10 --i=20 --n=200;
 
 # "Internet & Telecom"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=11 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=11 --i=20 --n=200;
 
 # "Sports & Fitness"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=12 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=12 --i=20 --n=200;
 
 # "Dining & Nightlife"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=13 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=13 --i=20 --n=200;
 
 # "Business & Industrial"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=14 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=14 --i=20 --n=200;
 
 # "Occasions & Gifts"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=15 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=15 --i=20 --n=200;
 
 # "Travel & Tourism"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=16 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=16 --i=20 --n=200;
 
 # "News, Media & Publications"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=17 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=17 --i=20 --n=200;
 
 # "Apparel"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=18 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=18 --i=20 --n=200;
 
 # "Beauty & Personal Care"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=19 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=19 --i=20 --n=200;
 
 # "Family & Community"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=20 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=20 --i=20 --n=200;
 
 # "Real Estate"
-python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=21 --i=2 --n=5;
+python hyperparameter_optimization/logistic_regression/hyperparam_opt_logistic_regression.py --c=21 --i=20 --n=200;
