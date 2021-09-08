ea645c9554f58421ef4477bb82f54fcba74726ba
enriquedlh97 <enriquedlh97@hotmail.com>
2021-09-08 12:41:36 -0500

    Sets up final training of baseline models

--- training_and_testing/test_baseline_models.sh
+++ training_and_testing/test_baseline_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python training_and_testing/test_baseline_model.py --s=0.05 --m='rf';
+python training_and_testing/test_baseline_model.py --s=1 --m='rf';
 
-python training_and_testing/test_baseline_model.py --s=0.05 --m='lr';
+python training_and_testing/test_baseline_model.py --s=1 --m='lr';
 
-python training_and_testing/test_baseline_model.py --s=0.05 --m='svm';
+python training_and_testing/test_baseline_model.py --s=1 --m='svm';
