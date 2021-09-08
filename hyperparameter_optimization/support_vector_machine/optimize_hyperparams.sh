c6e8835d36ad06d40022c073258af478f46f7a3f
enriquedlh97 <enriquedlh97@hotmail.com>
2021-09-08 06:03:26 -0500

    Adds script for hyperparam opt of all categories

---
+++ hyperparameter_optimization/support_vector_machine/optimize_hyperparams.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# "Health"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=0 --i=1 --n=2;
+
+# "Vehicles"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=1 --i=1 --n=2;
+
+# "Hobbies & Leisure"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=2 --i=1 --n=2;
+
+# "Food & Groceries"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=3 --i=1 --n=2;
+
+# "Retailers & General Merchandise"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=4 --i=1 --n=2;
+
+# "Arts & Entertainment"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=5 --i=1 --n=2;
+
+# "Jobs & Education"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=6 --i=1 --n=2;
+
+# "Law & Government"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=7 --i=1 --n=2;
+
+# "Home & Garden"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=8 --i=1 --n=2;
+
+# "Finance"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=9 --i=1 --n=2;
+
+# "Computers & Consumer Electronics"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=10 --i=1 --n=2;
+
+# "Internet & Telecom"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=11 --i=1 --n=2;
+
+# "Sports & Fitness"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=12 --i=1 --n=2;
+
+# "Dining & Nightlife"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=13 --i=1 --n=2;
+
+# "Business & Industrial"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=14 --i=1 --n=2;
+
+# "Occasions & Gifts"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=15 --i=1 --n=2;
+
+# "Travel & Tourism"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=16 --i=1 --n=2;
+
+# "News, Media & Publications"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=17 --i=1 --n=2;
+
+# "Apparel"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=18 --i=1 --n=2;
+
+# "Beauty & Personal Care"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=19 --i=1 --n=2;
+
+# "Family & Community"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=20 --i=1 --n=2;
+
+# "Real Estate"
+python hyperparameter_optimization/support_vector_machine/hyperparam_opt_support_vector_machine.py --c=21 --i=1 --n=2;
